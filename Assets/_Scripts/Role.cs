e331c734a9b44a2eee95b7eb03af44b6a44471eb
JianMaster <1447247475@qq.com>
2026-06-16 21:41:35 +0900

    添加字体

--- Assets/_Scripts/Role.cs
+++ Assets/_Scripts/Role.cs
@@ -1,29 +1,31 @@
 using UnityEngine;
 
 [RequireComponent(typeof(Animator))]
-public class Role : MonoBehaviour
-{
+public class Role : MonoBehaviour {
     [SerializeField] private Transform _lookTarget;
+    [SerializeField] private Transform _lookPlayerTarget;
+    private bool _isLookingAtPlayer;
+    public bool IslookingAtPlayer {
+        get => _isLookingAtPlayer;
+        set => _isLookingAtPlayer = value;
+    }
 
     private Animator _animator;
 
-    private void Awake()
-    {
+    private void Awake() {
         _animator = GetComponent<Animator>();
     }
 
-    private void OnAnimatorIK(int layerIndex)
-    {
+    private void OnAnimatorIK(int layerIndex) {
         if (_lookTarget == null) return;
 
+        var target = _isLookingAtPlayer ? _lookPlayerTarget : _lookTarget;
         _animator.SetLookAtWeight(0.8f, 0.2f, 0.8f, 0f, 0.6f);
-        _animator.SetLookAtPosition(_lookTarget.position);
+        _animator.SetLookAtPosition(target.position);
     }
 
-    public void PlayNod()
-    {
-        if (TryGetComponent<NodAnimation>(out var nodAnimation))
-        {
+    public void PlayNod() {
+        if (TryGetComponent<NodAnimation>(out var nodAnimation)) {
             nodAnimation.PlayNod();
         }
     }
